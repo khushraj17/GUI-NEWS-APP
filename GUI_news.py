60f8ddba9751a3db869e8b371bd4750f5bfa5564
khushraj17 <khushrajrane@gmail.com>
2025-12-06 17:44:23 +0530

    add scroll bar using canvas

--- GUI_news.py
+++ GUI_news.py
@@ -1,6 +1,6 @@
 from tkinter import *
 import requests
-from urllib.request import urlopen
+from urllib.request import urlopen, Request
 from PIL import ImageTk,Image
 import io
 import webbrowser
@@ -21,13 +21,24 @@ class news_GUI :
         self.root = Tk()
         self.root.title("NEWS APP")
         self.root.iconbitmap(r"resources\favicon.ico")
-        # self.root.geometry('350x600')
+        self.root.geometry('500x600')
         self.root.resizable(0,0)
-        self.root.config(bg="gray")
+        self.root.config(bg="#1e1e1e")
         
+        self.canvas = Canvas(self.root, bg="#1e1e1e", highlightthickness=0)
+        self.scrollbar = Scrollbar(self.root, orient="vertical", command=self.canvas.yview)
+        self.scrollbar.pack(side="right", fill="y")
+        self.canvas.pack(side="left", fill="both", expand=True)
+
+        self.canvas.configure(yscrollcommand=self.scrollbar.set)
+        self.frame = Frame(self.canvas, bg="#1e1e1e")
+        self.canvas.create_window((0, 0), window=self.frame, anchor="nw")
+
+        self.frame.bind("<Configure>", lambda e: self.canvas.configure(scrollregion=self.canvas.bbox("all")))
+
 
     def clear(self): 
-        for i in self.root.pack_slaves(): 
+        for i in self.frame.winfo_children(): 
             i.destroy()
 
     def load_news_item(self,index): 
@@ -46,39 +57,40 @@ class news_GUI :
             else:
                 raise Exception("No image")
         except:
-            im = Image.open("fallback.png").resize((350,250))
+            im = Image.open("fallback.png").resize((490,250))
 
         photo = ImageTk.PhotoImage(im)
 
-        lable = Label(self.root, image=photo)
+        lable = Label(self.frame, image=photo)
         lable.image = photo 
         lable.pack()
 
-        heading = Label(self.root,text= self.data["results"][index]["title"], bg='black',fg= 'white',wraplength=350,justify='center')
-        heading.pack(pady=(10,20))
+        heading = Label(self.frame,text= self.data["results"][index]["title"], bg='black',fg= 'white',wraplength=500,justify='center')
+        heading.pack(pady=(5,5),padx=(5,0))
         heading.config(font=('verdana',15))
 
-        details = Label(self.root,text= self.data["results"][index]["description"], bg='black',fg= 'white',wraplength=350,justify='center')
-        details.pack(pady=(10,20))
+        details = Label(self.frame,text= self.data["results"][index]["description"], bg='black',fg= 'white',wraplength=500,justify='center')
+        details.pack(pady=(5,5),padx=(5,0))
         details.config(font=('verdana',10))
         
 
-        frame = Frame(self.root,bg = 'black')
+        frame = Frame(self.frame,bg = "#1D1D1D")
         frame.pack(expand=True,fill=BOTH)
 
         if index !=  0:
-            prev = Button(frame,text="prev",width=16,height=3,command=lambda: self.load_news_item(index-1))
-            prev.pack(side=LEFT)
+            prev = Button(frame,text="prev",width=20,height=3,command=lambda: self.load_news_item(index-1))
+            prev.pack(side=LEFT,padx=(10,5))
 
-        read = Button(frame,text="Read more",width=16,height=3,command=lambda: self.open_link(self.data["results"][index]["link"]) )
-        read.pack(side=LEFT)
+        read = Button(frame,text="Read more",width=20,height=3,command=lambda: self.open_link(self.data["results"][index]["link"]) )
+        read.pack(side=LEFT,padx=5)
 
         if index != len(self.data["results"])-2: 
-            next= Button(frame,text="next",width=16,height=3,command=lambda: self.load_news_item(index+1))
-            next.pack(side=LEFT)
+            next= Button(frame,text="next",width=20,height=3,command=lambda: self.load_news_item(index+1))
+            next.pack(side=LEFT,padx=5)
 
     def open_link(self,url): 
-        webbrowser.open(url)
+        if url:
+            webbrowser.open(url)
 
 
 obj = news_GUI()
