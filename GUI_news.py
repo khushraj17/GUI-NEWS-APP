ae39de88af6b6b59538785c0d332130fb9ffbd19
khushraj17 <khushrajrane@gmail.com>
2025-12-06 16:06:24 +0530

    add img

--- GUI_news.py
+++ GUI_news.py
@@ -1,5 +1,8 @@
 from tkinter import *
 import requests
+from urllib.request import urlopen
+from PIL import ImageTk,Image
+import io
 
 
 class news_GUI : 
@@ -7,6 +10,8 @@ class news_GUI :
         self.data = requests.get("https://newsdata.io/api/1/latest?country=in&language=en&apikey=pub_c0f3643f166144ce908b709535b2a6c4").json()
         self.loadgui()
         self.load_news_item(0)
+        
+        self.root.mainloop()
 
     def loadgui(self): 
         self.root = Tk()
@@ -23,6 +28,20 @@ class news_GUI :
 
     def load_news_item(self,index): 
         self.clear()
+
+        img_url = self.data["results"][index]["image_url"]
+
+        if img_url:
+            rawdata = urlopen(img_url).read()
+            im = Image.open(io.BytesIO(rawdata)).resize((350,250))
+        else:
+            im = Image.open("fallback.png").resize((350,250))
+
+        photo = ImageTk.PhotoImage(im)
+
+        lable = Label(self.root, image=photo)
+        lable.pack()
+
         heading = Label(self.root,text= self.data["results"][index]["title"], bg='black',fg= 'white',wraplength=350,justify='center')
         heading.pack(pady=(10,20))
         heading.config(font=('verdana',15))
@@ -43,6 +62,7 @@ class news_GUI :
 
         next= Button(frame,text="next",width=16,height=3)
         next.pack(side=LEFT)
-        self.root.mainloop()
+
+
 
 obj = news_GUI()
